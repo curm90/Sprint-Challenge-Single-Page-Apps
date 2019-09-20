96d1e7c7e9649e0479d7c0f833fbce36f6a85797
Liam Sutton <devliam90@gmail.com>
2019-09-20 12:11:24 +0100

    hook up welcome page and character routes

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -1,16 +1,21 @@
-import React from "react";
+import React from 'react';
+import rick from '../images/rick-morty.jpeg';
+import styled from 'styled-components';
+
+const Section = styled.section`
+  img {
+    width: 100%;
+    box-shadow: 2px 2px 4px black;
+  }
+`;
 
 export default function WelcomePage() {
   return (
-    <section className="welcome-page">
+    <Section className='welcome-page'>
       <header>
         <h1>Welcome to the ultimate fan site!</h1>
-        <img
-          className="main-img"
-          src="https://rickandmortyapi.com/api/character/avatar/1.jpeg"
-          alt="rick"
-        />
+        <img className='main-img' src={rick} alt='rick' />
       </header>
-    </section>
+    </Section>
   );
 }
