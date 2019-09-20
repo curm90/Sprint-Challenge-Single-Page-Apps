7f75fe2b8e9409d394f52272bc1871d2e9e1807b
Liam Sutton <devliam90@gmail.com>
2019-09-20 13:32:00 +0100

    add character page styles

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,11 +1,41 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Div = styled.div`
+  border: 1px solid #e2e2e2;
+  box-shadow: 1px 1px 4px #e2e2e2;
+  border-radius: 3px;
+  width: 30%;
+  margin: 1rem 0;
+
+  img {
+    width: 100%;
+    box-shadow: 0px 6px 4px #e2e2e2;
+  }
+
+  .info {
+    padding: 0.6rem;
+
+    ul {
+      padding: 0;
+      li {
+        list-style: none;
+        line-height: 1.5;
+
+        :nth-child(1) {
+          color: #bcbcbc;
+        }
+      }
+    }
+  }
+`;
 
 export default function CharacterCard({ character }) {
   return (
-    <div>
+    <Div>
       <div key={character.id}>
         <img src={character.image} alt={character.name} />
-        <div>
+        <div className='info'>
           <h2>{character.name}</h2>
           <ul>
             <li>
@@ -16,6 +46,6 @@ export default function CharacterCard({ character }) {
           </ul>
         </div>
       </div>
-    </div>
+    </Div>
   );
 }
